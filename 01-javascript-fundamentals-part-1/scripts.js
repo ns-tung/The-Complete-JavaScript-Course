7103dd3cd98a1729d168fe59f15b008e9a65d218
Nguyen Son Tung <tungns.wd@gmail.com>
2024-03-09 17:25:04 +0700

    Values and Variables

--- 01-javascript-fundamentals-part-1/scripts.js
+++ 01-javascript-fundamentals-part-1/scripts.js
@@ -1,5 +1,48 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Tung Nguyen");
+console.log(34);
+
+// VALUES AND VARIABLES
+
+let firstName = "Tung";
+let lastName = "Nguyen";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let tung_nguyen = "TN";
+let $function = "34";
+
+// Conventions and rules for naming variables:
+//  should be only using "camelCase", ex: first, firstName, firstNamePerson
+//  should not start with an uppercase, ex: Person (this is a convention, this naming will used for constants and OOP)
+
+// TWO SYMBOLS are ALLOWED when naming variables: "_" and "$", besides LETTERS and NUMBERS.
+
+// Illegal variables name:
+//  start with number: 3years
+//  jonas&matilda (& is a illegal symbol)
+//  using keywords: new, function
+
+// Should not using keyword: name (conventions)
+
+// Should be:
+let myFirstJob = "Technician";
+let myCurrentJob = "Freelancer";
+
+// Should not:
+let job1 = "Technician";
+let job2 = "Freelancer";
+
+// Practice Assignments:
+// 1. Declare variables called country, continent and population and assign their values according to your own country(population in millions).
+// 2. Log their values to the console.
+
+let country = "Viet Nam";
+let continent = "Asia";
+let population = "100"; // in millions
+console.log(country);
+console.log(continent);
+console.log(population);
