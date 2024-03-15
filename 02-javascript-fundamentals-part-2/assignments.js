d196515bb439bfadd9543a40dc585f3b010c6708
Nguyen Son Tung <tungns.wd@gmail.com>
2024-03-15 16:13:49 +0700

    Functions

--- 02-javascript-fundamentals-part-2/assignments.js
+++ 02-javascript-fundamentals-part-2/assignments.js
@@ -1,3 +1,17 @@
-/* ASSIGNMENTS ==================================================
+/* FUNCTIONS ==================================================
     
+    1. Write a function called describeCountry which takes three parameters: country, population and capitalCity. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'.
+
+    2. Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.  
 */
+
+"use strict";
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const describeVietnam = describeCountry("Vietnam", "100", "Hanoi");
+const describeFinland = describeCountry("Finland", "6", "Helsinki");
+const describePortugal = describeCountry("Portugal", "10", "Lisbon");
+console.log(describeVietnam + "\n\n" + describeFinland + "\n\n" + describePortugal);
--- 02-javascript-fundamentals-part-2/theories.js
+++ 02-javascript-fundamentals-part-2/theories.js
@@ -23,3 +23,34 @@ if (hasDriversLicense) console.log("You can drive 😀");
 
 const interface = "Audio"; // 🚫 SyntaxError: Unexpected strict mode reserved word
 const private = 34; // 🚫 SyntaxError: Unexpected strict mode reserved word
+
+/* FUNCTIONS ==================================================
+    
+    💠 Functions are one of the fundamental building blocks in JavaScript. A function in JavaScript is similar to a procedure – a set of statements that performs a task or calculates a value. Still, for a procedure to qualify as a function, it should take some input and return an output where there is some apparent relationship between the input and the output. To use a function, you must define it somewhere in the scope you wish to call it.
+
+    💠 Generally speaking, a function is a "subprogram" that can be called by code external (or internal, in the case of recursion) to the function. Like the program, a function comprises a sequence of statements called the function body. Values can be passed to a function as parameters, returning a value.
+
+    💠 In JavaScript, functions are first-class objects because they can be passed to other functions, returned from functions, and assigned to variables and properties. They can also have properties and methods just like any other object. What distinguishes them from other objects is that functions can be called.
+*/
+
+const firstName = "Tung";
+function logger() {
+  console.log(`My name is ${firstName}`);
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("34"); // Number() is a build-in function.
