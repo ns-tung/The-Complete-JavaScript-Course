b9f7fea0226cd8616bbec88d9c3de7f8e5ae1601
Nguyen Son Tung <tungns.wd@gmail.com>
2024-04-08 14:00:47 +0700

    JavaScript in the Browser: DOM and Events Fundamentals

---
+++ _assets/scripts.js
@@ -0,0 +1,57 @@
+import {
+  computePosition,
+  flip,
+  shift,
+  offset,
+} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.3/+esm';
+
+window.onload = function () {
+
+  document.addEventListener('contextmenu', e => e.preventDefault());
+
+  const tooltipElement = document.querySelectorAll('[role="tooltip"]');
+
+  tooltipElement.forEach(tooltip => {
+    const float = tooltip.nextElementSibling; // float is the next sibling
+
+    function setUpTooltip() {
+      computePosition(tooltip, float, {
+        placement: 'top',
+        middleware: [offset(4), flip(), shift({ padding: 5 })],
+      }).then(({ x, y }) => {
+        Object.assign(float.style, {
+          left: `${x}px`,
+          top: `${y}px`,
+        });
+      });
+    }
+
+    function showTooltip() {
+      float.style.display = 'block';
+      setUpTooltip();
+    }
+
+    function hideTooltip() {
+      float.style.display = 'none';
+    }
+
+    [
+      ['mouseenter', showTooltip],
+      ['mouseleave', hideTooltip],
+      ['focus', showTooltip],
+      ['blur', hideTooltip],
+    ].forEach(([event, listener]) =>
+      tooltip.addEventListener(event, listener)
+    );
+  });
+
+  const links = document.querySelectorAll('a.token.url-link[href]');
+
+  links.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      window.open(link.href, '_blank');
+    });
+  });
+
+};
