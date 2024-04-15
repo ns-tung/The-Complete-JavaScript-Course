450be7ae8272aea3a5541c88ed7a11d20e1b71b0
Nguyen Son Tung <tungns.wd@gmail.com>
2024-04-15 15:52:51 +0700

    The call() and apply() Methods

--- a-closer-look-at-functions/scripts.js
+++ a-closer-look-at-functions/scripts.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* DEFAULT PARAMETERS ==================================================
 
     Introduced on ES6
@@ -131,3 +133,57 @@ greet('Hello')('Jonas'); // Hello Jonas
 const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Jonas'); // Hi Jonas
+
+/* THE CALL AND APPLY METHODS ==================================================
+    
+    call(), apply()
+
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann'); // Jonas Schmedtmann booked a seat on Lufthansa flight LH239
+lufthansa.book(635, 'John Smith'); // John Smith booked a seat on Lufthansa flight LH635
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams'); 👉 the this keyword points to undefined
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
