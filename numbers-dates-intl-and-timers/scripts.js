d6798a6658c43ffceb3cde8b086b017b0296e579
Nguyen Son Tung <tungns.wd@gmail.com>
2024-04-22 15:54:32 +0700

    Math and Rounding

--- numbers-dates-intl-and-timers/scripts.js
+++ numbers-dates-intl-and-timers/scripts.js
@@ -38,3 +38,47 @@ console.log(Number.isFinite(23 / 0)); // false
 console.log(Number.isInteger(23)); // true
 console.log(Number.isInteger(23.0)); // true
 console.log(Number.isInteger(23 / 0)); // false
+
+/* MATH AND ROUNDING ==================================================
+
+    .sqrt(), max(), min(), .PI, .random(), .trunc(), .round(), .ceil(), .floor()
+
+*/
+
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+console.log(8 ** (1 / 3)); // 2
+
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+
+// Rounding integers
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor('23.9')); // 23
+
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Rounding decimals
+// toFixed() does not do type coercion
+console.log((2.7).toFixed(0)); // 3 -> string
+console.log((2.7).toFixed(3)); // 2.700 -> string
+console.log((2.345).toFixed(2)); // 2.35 -> string
+console.log(+(2.345).toFixed(2)); // 2.35 -> number
