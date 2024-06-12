6dd6fcdabcc424e8ac2ee58f634e89d19e1680c2
Nguyen Son Tung <tungns.wd@gmail.com>
2024-06-12 16:46:25 +0700

    Prototypes

--- object-oriented-programming-with-javascript/scripts.js
+++ object-oriented-programming-with-javascript/scripts.js
@@ -21,3 +21,28 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person); // true
+
+/* PROTOTYPES ==================================================
+
+*/
+
+Person.prototype.calcAge = function () {
+    console.log(2034 - this.birthYear);
+}
+
+jonas.calcAge(); // 43
+matilda.calcAge(); // 17
+
+console.log(jonas.__proto__); // {calcAge: f}
+console.log(jonas.__proto__ === Person.prototype); // true
+
+console.log(Person.prototype.isPrototypeOf(jonas)); // true
+console.log(Person.prototype.isPrototypeOf(matilda)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species); // Homo Sapiens Homo Sapiens
+console.log(jonas.hasOwnProperty('firstName')); // true
+console.log(jonas.hasOwnProperty('species')); // false
+
+console.log(Person.prototype); // ▶ {species: 'Homo Sapiens', calcAge: ƒ}
