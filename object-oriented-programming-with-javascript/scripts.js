bca295b320a7954ee609340ad53ff5e960399169
Nguyen Son Tung <tungns.wd@gmail.com>
2024-06-12 17:48:50 +0700

    Prototypal Inheritance and The Prototype Chain

--- object-oriented-programming-with-javascript/scripts.js
+++ object-oriented-programming-with-javascript/scripts.js
@@ -46,3 +46,26 @@ console.log(jonas.hasOwnProperty('firstName')); // true
 console.log(jonas.hasOwnProperty('species')); // false
 
 console.log(Person.prototype); // ▶ {species: 'Homo Sapiens', calcAge: ƒ}
+
+/* PROTOTYPAL INHERITANCE AND THE PROTOTYPE CHAIN ==================================================
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
+
+*/
+
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [1, 3, 5, 5, 7, 9, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+Array.prototype.uniqueArray = function () {
+    return [...new Set(this)]
+};
+
+console.log(arr.uniqueArray()); // ▶ (5) [1, 3, 5, 7, 9]
+console.dir(x => x + 1);
