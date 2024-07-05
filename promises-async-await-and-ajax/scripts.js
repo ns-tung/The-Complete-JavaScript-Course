7355a67b5a0546362b71252d7ee4bff1ce282415
Nguyen Son Tung <tungns.wd@gmail.com>
2024-07-05 17:55:59 +0700

    Handling Rejected Promises

--- promises-async-await-and-ajax/scripts.js
+++ promises-async-await-and-ajax/scripts.js
@@ -2,7 +2,46 @@
 
 const restCountriesName = 'https://restcountries.com/v3.1/name/';
 const restCountriesCode = 'https://restcountries.com/v3.1/alpha/';
-const countriesRow = document.querySelector('.countries');
+const spn = document.querySelector('.spinner');
+const btn = document.querySelector('.view-country');
+let countriesRow = document.querySelector('.countries');
+
+const removeSpinner = function () {
+  spn.classList.add('d-none');
+}
+
+const clearCountries = function () {
+  if (countriesRow.innerHTML === '') return;
+  countriesRow.innerHTML = ''
+};
+
+const renderCountry = function (data, neighbor = '') {
+  const html = `
+    <div class="col d-flex flex-wrap align-items-stretch country${neighbor}">
+      <div class="card shadow-lg border-0 overflow-hidden">
+        <img class="card-img-top" src="${data.flags.svg}" alt="${data.flags.alt || `The flag of ${data.name.common}`}" />
+        <div class="card-body bg-gray-400">
+          <div class="d-flex align-items-center justify-content-between mb-3">
+            <h2 class="card-title m-0">${data.name.common}
+              ${neighbor !== '' ? `<button class="btn btn-sm px-2 ms-2 view-country" data-country="${data.name.common.toLowerCase()}">View</button>` : ``}
+            </h2>
+            <p class="badge bg-primary-lt pb-1 m-0">${data.region.toUpperCase()}</p>
+          </div>
+          <p class="card-text"><span>👫 </span>${(+data.population / 1000000).toFixed(2)}M people</p>
+          <p class="card-text"><span>🗣️ </span>${Object.values(data.languages).join(', ')}</p>
+          <p class="card-text"><span>💰 </span>${Object.keys(data.currencies).join(', ')}</p>
+        </div>
+        ${neighbor !== '' ? `<div class="ribbon ribbon-bottom bg-teal"><p class="card-text">Neighbor</p></div>` : ``}
+      </div>
+    </div>`;
+  countriesRow.insertAdjacentHTML('beforeend', html);
+}
+
+const renderError = function (msg) {
+  clearCountries();
+  msg = `<p class="pt-3">${'🔻 ' + msg}</p>`;
+  countriesRow.insertAdjacentHTML('afterbegin', msg);
+}
 
 /* Asynchronous JavaScript, AJAX and APIs ==================================================
 
@@ -38,26 +77,6 @@ const getCountry = function (country) {
 // getCountry('vietnam'); getCountry('lao'); getCountry('cambodia'); getCountry('usa'); getCountry('germany'); getCountry('portugal');
 
 /* Callback Hell ================================================== */
-
-const renderCountry = function (data, neighbor = '') {
-  const html = `
-    <div class="col d-flex flex-wrap align-items-stretch country${neighbor}">
-      <div class="card shadow-lg border-0 overflow-hidden">
-        <img class="card-img-top" src="${data.flags.svg}" alt="${data.flags.alt || `The flag of ${data.name.common}`}" />
-        <div class="card-body bg-gray-400">
-          <div class="d-flex align-items-center justify-content-between mb-3">
-            <h2 class="card-title m-0">${data.name.common}</h2>
-            <p class="badge bg-primary-lt pb-1 m-0">${data.region.toUpperCase()}</p>
-          </div>
-          <p class="card-text"><span>👫 </span>${(+data.population / 1000000).toFixed(2)}M people</p>
-          <p class="card-text"><span>🗣️ </span>${Object.values(data.languages).join(', ')}</p>
-          <p class="card-text"><span>💰 </span>${Object.keys(data.currencies).join(', ')}</p>
-        </div>
-        ${neighbor !== '' ? `<div class="ribbon ribbon-bottom bg-teal"><p class="card-text">Neighbor</p></div>` : ``}
-      </div>
-    </div>`;
-  countriesRow.insertAdjacentHTML('beforeend', html);
-}
 
 const getCountryAndNeighbor = function (country) {
 
@@ -148,4 +167,57 @@ const getCountryDataRecursive = function (country, neighbor = '') {
     });
 };
 
-getCountryDataRecursive('finland');
+// getCountryDataRecursive('finland');
+
+/* Handling Rejected Promises ================================================== */
+
+const getCountryWithErr = function (country) {
+  fetch(restCountriesName + country)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data[0])
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+      if (!neighbor) return;
+      return fetch(restCountriesCode + neighbor);
+    })
+    .then(res => res.json())
+    .then(data => {
+      renderCountry(data[0], ' neighbor')
+    }).finally(() => removeSpinner());
+}
+
+// btn.addEventListener('click', function () {
+//   clearCountries();
+//   spn.classList.remove('d-none');
+//   getCountryWithErr('viet');
+// });
+
+const getCountryWithError = function (country, neighbor = '') {
+  fetch(neighbor === '' ? restCountriesName + country : restCountriesCode + country)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0], neighbor);
+      if (!data[0].borders || neighbor !== '') return;
+      data[0].borders.map(country => getCountryWithError(country, ' neighbor'))
+    }).catch(err => {
+      console.error(err + ' 🔺');
+      renderError(`<strong>Error</strong>: ${err.message}. Try again!`);
+    }).finally(() => removeSpinner());
+}
+
+getCountryWithError('norway');
+
+btn.addEventListener('click', function () {
+  clearCountries();
+  spn.classList.remove('d-none');
+  getCountryWithError('finland');
+});
+
+countriesRow.addEventListener('click', e => {
+  const viewCountry = e.target;
+  if (!viewCountry.classList.contains('view-country')) return;
+  clearCountries();
+  spn.classList.remove('d-none');
+  getCountryWithError(viewCountry.dataset.country);
+});
