1e6bf3480bd62e7bd21e25fe940938d920ee5022
Nguyen Son Tung <tungns.wd@gmail.com>
2024-07-10 13:16:55 +0700

    Asynchronous JavaScript: Coding Challenge 01

--- promises-async-await-and-ajax/scripts.js
+++ promises-async-await-and-ajax/scripts.js
@@ -280,3 +280,86 @@ countriesRow.addEventListener('click', e => {
   spn.classList.remove('d-none');
   getCountryAndThrowError(viewCountry.dataset.country);
 });
+
+/* Coding Challenge #1 ==================================================
+
+  In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+  Here are your tasks:
+
+  PART 1
+
+  1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+  2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+  The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+  3. Once you have the data, take a look at it in the console to see all the attributes that you received about the provided location. Then, using this data, log a message like this to the console: 'You are in Berlin, Germany'
+  4. Chain a .catch method to the end of the promise chain and log errors to the console
+  5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+  PART 2
+  
+  6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+  7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+  TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+  TEST COORDINATES 2: 19.037, 72.873
+  TEST COORDINATES 2: -33.933, 18.474
+
+*/
+
+const countriesReverse = document.querySelector('.reverse-countries');
+
+const renderCountryReverse = function (data) {
+  const html = `
+    <div class="col d-flex flex-wrap align-items-stretch country">
+      <div class="card shadow-lg border-0 overflow-hidden">
+        <img class="card-img-top" src="${data.flags.svg}" alt="${data.flags.alt || `The flag of ${data.name.common}`}" />
+        <div class="card-body bg-gray-400">
+          <div class="d-flex align-items-center justify-content-between mb-3">
+            <h2 class="card-title m-0">${data.name.common}</h2>
+            <p class="badge bg-primary-lt pb-1 m-0">${data.region.toUpperCase()}</p>
+          </div>
+          <p class="card-text"><span>👫 </span>${(+data.population / 1000000).toFixed(2)}M people</p>
+          <p class="card-text"><span>🗣️ </span>${Object.values(data.languages).join(', ')}</p>
+          <p class="card-text"><span>💰 </span>${Object.keys(data.currencies).join(', ')}</p>
+        </div>
+      </div>
+    </div>`;
+  countriesReverse.insertAdjacentHTML('beforeend', html);
+}
+
+const whereAmI = function (lat, lng) {
+  const apiReverse = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&accept-language=en`;
+  fetch(apiReverse)
+    .then(res => {
+      if (!res.ok) throw new Error(`(${res.status}) Problem with geocoding.`);
+      return res.json();
+    })
+    .then(data => {
+      const { city, country, country_code } = data.address;
+      console.log(`You are in ${city}, ${country}`)
+      return fetch(restCountriesCode + country_code)
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`(${res.status}) Country not found.`);
+      return res.json();
+    })
+    .then(data => renderCountryReverse(data[0]))
+    .catch(err => console.log('🚫 ' + err));
+}
+
+const whereAmIRefactor = function (lat, lng) {
+  const apiReverse = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&accept-language=en`;
+  fetchCountry(apiReverse, 'Problem with geocoding.')
+    .then(data => {
+      const { city, country, country_code } = data.address;
+      console.log(`You are in ${city}, ${country}`)
+      return fetchCountry(restCountriesCode + country_code, 'Country not found.')
+    })
+    .then(data => renderCountryReverse(data[0]))
+    .catch(err => console.log('🚫 ' + err));
+}
+
+whereAmIRefactor(52.508, 13.381);
+whereAmIRefactor(19.037, 72.873);
+whereAmIRefactor(-33.933, 18.474);
